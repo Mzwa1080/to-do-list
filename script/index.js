6477ad8f5b7d7d737d968dea1bc701bd52d9d4ca
Mzwa1080 <mshawnlouw@gmail.com>
2023-12-02 02:08:59 +0200

    added some few changes

--- script/index.js
+++ script/index.js
@@ -1,31 +1,39 @@
 let tasks = [];
 
 function addTask() {
-    let input = document.getElementById('taskInput');
-    let taskName = input.value.trim();
-
-    if (taskName.length === 0 || taskName.length <= 3) {
-        alert('Task must not be empty or have less than 3 characters.');
-    }
-
-    let capitalizedTask = taskName.charAt(0).toUpperCase() + taskName.slice(1);
-    // console.log(capitalizedTask);
-    document.getElementById('taskList').innerText = capitalizedTask 
-
+  let input = document.getElementById('taskInput');
+  let taskName = input.value.trim();
+  console.log(taskName);
+  let capitaLize = taskName.charAt(0).toUpperCase() + taskName.slice(1);
+  
     let addedTask = {
-        id: tasks.length + 1,
-        name: capitalizedTask,
-        createdDate: new Date().toDateString(),
-        completed : false
+      id: tasks.length + 1,
+      name: capitaLize,
+      createdDate: new Date().toDateString(),
+      completed: false
     };
-    // console.log(addedTask); //Lets check out addedTask add if there are things there
-    tasks.push(addedTask);
-    //   Then we clear out input 
-    input.value = '';
 
-      getTasks();
+  if (taskName.length === 0 || taskName.length <= 3) {
+    alert('Task must not be empty or have less than 3 words.');
+  }
+ // console.log(addedTask); //Lets check out addedTask add if there are things there
+    tasks.push(addedTask);
+
+  document.getElementById('taskList').innerText = capitaLize
+// NOW LETS ADD IT TO LOCALSTORAGE
+  localStoragee()
+// Then we clear out input 
+  input.value = '';
+
+
+  getTasks();
+}
+//I will call this LocalStoragefunction when page refreshes and when adding tasks
+function localStoragee() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+// my btn
 document.querySelector('[btn-add]').addEventListener('click', addTask)
 
 
@@ -36,25 +44,56 @@ function getTasks() {
   tasks.forEach(addedTask => {
 
     let listItem = document.createElement('li');
-    listItem.innerText = addedTask.name + '  Date created: ' + addedTask.createdDate;
-
+    listItem.innerHTML = `<span>${addedTask.name}</span> `;
 
     if (addedTask.completed) {
       listItem.classList.add('completed');
     }
+// Lets create our checkbox and delete buttons
     let checkbox = document.createElement('input');
+    let deleteButton = document.createElement('button');
+
     checkbox.type = 'checkbox';
     checkbox.checked = addedTask.completed;
-
-
-    checkbox.addEventListener('change', () => toggleTaskCompletion(addedTask.id));
+    deleteButton.textContent = 'Delete';
+    
     listItem.prepend(checkbox);
+    listItem.prepend(deleteButton)
     taskList.appendChild(listItem);
+    deleteButton.addEventListener('click', () => deleteTask(addedTask.id));
+    checkbox.addEventListener('change', () => toggleTaskCompletion(addedTask.id));
   });
+
+// Joel also wanted the task to be striked through/linethrough thats styling in js
+// So if addedTask.completed is true. strike though a line
+if(addTask.completed){
+  taskName.style.textDecoration = 'line-through';
+ }
+
+
 }
 
-// I NEED TO DO THE LOCALSTORAGE 
+// This function must delete a selected item from the object, selected by id 
+function deleteTask(id) {
+  tasks = tasks.filter(task => task.id !== id);
 
+  localStoragee();
+  getTasks();
+}
+
+
+// Function to toggle task completion
+function toggleTaskCompletion(id) {
+  const taskIndex = tasks.findIndex(task => task.id === id);
+
+  if (taskIndex !== -1) {
+    tasks[taskIndex].completed = !tasks[taskIndex].completed;
+    getTasks(); // Update the tasks display
+  }
+}
+
+
+// I NEED TO DO THE LOCALSTORAGE 
 
 
 
